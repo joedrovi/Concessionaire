e22bec96b29a66769c15da1eadc293687e9746af
Jose Bernal <maxbernal@localhost.localdomain>
2012-05-08 21:22:25 -0500

    Hola mundo GIT

--- Concessionaire/src/concessionaire/Concessionaire.java
+++ Concessionaire/src/concessionaire/Concessionaire.java
@@ -14,6 +14,6 @@ public class Concessionaire {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Mundo GIT");
     }
 }
