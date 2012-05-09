df54561e6ce965d9b386d28f9eeb3601c04b9aa7
Claudia Santamaria <mcsan16@gmail.com>
2012-05-08 21:45:38 -0500

    Esto es una prueba

---
+++ Concessionaire/src/GUI/Widget.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author claito
+ */
+public class Widget extends JFrame {
+    public Widget() {
+        super("Widget");
+        setSize(640, 480);
+        setVisible(true);
+    }
+    
+}
