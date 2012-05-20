fb72668e3ea3c32934b583e57dd6e120d3368db5
Jose Bernal <maxbernal@localhost.localdomain>
2012-05-20 09:29:42 -0500

    Some changes

--- Concessionaire/src/Logic/Client.java
+++ Concessionaire/src/Logic/Client.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package DAO;
+package Logic;
 
 /**
  *
  * @author maxbernal
  */
-public class Cliente {
+public class Client {
     
 }
