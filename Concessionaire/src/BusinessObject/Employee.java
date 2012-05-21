35cb45fa335cdc881941cc3e0d05638b22c5e815
Enrique Jimenez <kikesiano@gmail.com>
2012-05-21 17:30:49 -0500

    He terminado el BusinessObject de Empleado

--- Concessionaire/src/BusinessObject/Employee.java
+++ Concessionaire/src/BusinessObject/Employee.java
@@ -10,4 +10,73 @@ package BusinessObject;
  */
 public class Employee {
     
+    private String id,nombre,telefono,email,cargo,direccion;
+    private char genero;
+    private double salario;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }   
+     
+    
 }
