b7312f13b5f31f9f4f426b73fe821b56de91c8f9
Enrique Jimenez <kikesiano@gmail.com>
2012-05-21 17:18:08 -0500

    He terminado el BusinessObject de vehiculo

--- Concessionaire/src/BusinessObject/Car.java
+++ Concessionaire/src/BusinessObject/Car.java
@@ -4,10 +4,85 @@
  */
 package BusinessObject;
 
-/**
- *
- * @author Chavelo
- */
 public class Car {
+    
+    private String numChasis,numMotor,color,numSerie,marca,anioFabricacion,anioGarantia;
+    private double costo;
+    private int cilindraje;
+
+    public String getAnioFabricacion() {
+        return anioFabricacion;
+    }
+
+    public void setAnioFabricacion(String anioFabricacion) {
+        this.anioFabricacion = anioFabricacion;
+    }
+
+    public String getAnioGarantia() {
+        return anioGarantia;
+    }
+
+    public void setAnioGarantia(String anioGarantia) {
+        this.anioGarantia = anioGarantia;
+    }
+
+    public int getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(int cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNumChasis() {
+        return numChasis;
+    }
+
+    public void setNumChasis(String numChasis) {
+        this.numChasis = numChasis;
+    }
+
+    public String getNumMotor() {
+        return numMotor;
+    }
+
+    public void setNumMotor(String numMotor) {
+        this.numMotor = numMotor;
+    }
+
+    public String getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(String numSerie) {
+        this.numSerie = numSerie;
+    }
+    
+    
+    
     
 }
