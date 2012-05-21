f0601232b605977e1875bd05809b6a64dc8f3317
Jose Bernal <maxbernal@localhost.localdomain>
2012-05-21 17:34:23 -0500

    Some little changes

--- Concessionaire/src/BusinessObject/Client.java
+++ Concessionaire/src/BusinessObject/Client.java
@@ -6,5 +6,67 @@ package BusinessObject;
  * @author Jose Bernal
  */
 public class Client {
-    
+    private String id;
+    private String tipoId;
+    private String nombre;
+    private String genero;
+    private String direccion;
+    private String telefono;
+    private String email;
+
+    public void setTipoId(String tipoId) {
+        this.tipoId = tipoId;
+    }
+
+    public String getTipoId() {
+        return tipoId;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
 }
