80ebf7c8aa717fdfa6076e8df0907b23e961f7b6
Claudia Santamaria <mcsan16@gmail.com>
2012-05-22 00:01:55 -0500

    Provider class done

--- Concessionaire/src/BusinessObject/Provider.java
+++ Concessionaire/src/BusinessObject/Provider.java
@@ -9,5 +9,49 @@ package BusinessObject;
  * @author Chavelo
  */
 public class Provider {
-    
+    private String nit;
+    private String razonSocial;
+    private String telefono;
+    private String direccion;
+    private String email;
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
