3c59205361bee95b83311957b2780a79ccfbf90a
Claudia Santamaria <mcsan16@gmail.com>
2012-05-21 21:47:48 -0500

    New class added

--- Concessionaire/src/BusinessObject/Accesory.java
+++ Concessionaire/src/BusinessObject/Accesory.java
@@ -9,5 +9,30 @@ package BusinessObject;
  * @author Chavelo
  */
 public class Accesory {
-    
+    private String serial, tipo;
+    private double costo;
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
